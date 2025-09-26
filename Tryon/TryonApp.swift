dcb22fde759c5d42edac6e492d0fbc14192a2a37
Eric Tan <erictanjr21@gmail.com>
2025-09-26 15:10:58 +0800

    Initial Commit

---
+++ Tryon/TryonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryonApp.swift
+//  Tryon
+//
+//  Created by Eric Tan Jun Ren on 26/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TryonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
